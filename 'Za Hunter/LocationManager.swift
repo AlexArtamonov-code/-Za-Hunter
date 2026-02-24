b93f1c52bf766a86b44a3a5758dbbdbbda8c9d56
Alex Artamonov <aartamonov@bsd220.org>
2026-02-24 10:41:51 -0600

    Added user location

---
+++ 'Za Hunter/LocationManager.swift
@@ -0,0 +1,24 @@
+//
+//  LocationManager.swift
+//  'Za Hunter
+//
+//  Created by Alex Artamonov on 2/23/26.
+//
+
+import Foundation
+import CoreLocation
+
+@Observable
+class LocationManager: NSObject, CLLocationManagerDelegate {
+    var locationManager = CLLocationManager()
+    
+    
+    override init(){
+        super.init()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    
+}

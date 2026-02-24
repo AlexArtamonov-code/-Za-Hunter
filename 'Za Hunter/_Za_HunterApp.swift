a6a98ebc8830801df365f216c983fbd3afc88b48
Alex Artamonov <aartamonov@bsd220.org>
2026-02-23 19:45:00 -0600

    Initial Commit

---
+++ 'Za Hunter/_Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  _Za_HunterApp.swift
+//  'Za Hunter
+//
+//  Created by Alex Artamonov on 2/23/26.
+//
+
+import SwiftUI
+
+@main
+struct _Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

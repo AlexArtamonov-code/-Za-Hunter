5867b7afb9fb7ca2d0ac7eb2c81372e0fe16c727
Alex Artamonov <aartamonov@bsd220.org>
2026-02-24 22:16:10 -0600

    Added website link and enabled directions

--- 'Za Hunter/LocationDetailsView.swift
+++ 'Za Hunter/LocationDetailsView.swift
@@ -20,7 +20,20 @@ struct LocationDetailsView: View {
             Text(mapItem.placemark.name!).font(.title).bold()
             Text(address).padding()
             Text(mapItem.phoneNumber!).padding()
-            Spacer()
+            if let url = mapItem.url {
+                Link("Visit Website", destination: url).padding()
+            }
+            
+            Button(action: {
+                let latitude = mapItem.placemark.coordinate.latitude
+                let longitude = mapItem.placemark.coordinate.longitude
+                let url = URL(string: "http://maps.apple.com/?q=\(latitude),\(longitude)")!
+                    if UIApplication.shared.canOpenURL(url) {
+                        UIApplication.shared.open(url)
+                    }
+            }, label:{
+                Text("Get Directions")
+            })
         }
     }
 }
